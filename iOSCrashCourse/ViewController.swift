29d792093aabdd2c88ccb7a12ab83b4abe988790
Efrain <jesusayllonroa@gmail.com>
2016-06-02 07:56:06 -0500

    Added Mood slider WITH EMOJIS instead of percentage

--- iOSCrashCourse/ViewController.swift
+++ iOSCrashCourse/ViewController.swift
@@ -11,34 +11,69 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var totalTextField :UITextField!
-    @IBOutlet var tipTextField :UITextField!
     @IBOutlet var tipLabel :UILabel!
+    @IBOutlet var tipSlider :UISlider!
+    @IBOutlet var moodLabel :UILabel!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tipSlider.addTarget(self, action: #selector(sliderValueChanged), forControlEvents: .ValueChanged)
+        
     }
     
-    @IBAction func calculateTipButtonPressed() {
+    
+    
+    
+    func sliderValueChanged() {
         
-        let totalAmount = Double(self.totalTextField.text!)
-        let tipPercentage = Double(self.tipTextField.text!)
-        
-        let tipAmount = totalAmount! * (tipPercentage!/100)
-        
-        self.tipLabel.text = "\(tipAmount)"
-        
-        print(self.totalTextField.text!)
-        
+        let total = Double(self.totalTextField.text!)
+        var tip = 0.0
+
+        switch self.tipSlider.value {
+            
+        case 0..<25:
+            self.tipSlider.value = 0
+            tip = 0
+            self.tipLabel.text = "\(tip)"
+            self.moodLabel.text = "😡"
+            
+            
+        case 25..<51:
+            tip = 5/100
+            self.tipLabel.text = "\(total! * tip)"
+            self.tipSlider.value = 25
+            self.moodLabel.text = "😐"
+            
+            
+            
+        case 51..<75:
+            tip = 10/100
+            self.tipLabel.text = "\(total! * tip)"
+            self.tipSlider.value = 50
+            self.moodLabel.text = "😋"
+            print("50..74")
+            
+        case 75..<101:
+            tip = 15/100
+            self.tipLabel.text = "\(total! * tip)"
+            self.tipSlider.value = 75
+            self.moodLabel.text = "😀"
+            print("75..100")
+            
+        default:
+            print("default")
+            
+        }
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
